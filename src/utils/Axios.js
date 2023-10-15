730e98f29649a9a4c412a9123fe8935908b95544
MUHAMMED HISHAM AC <122851090+hishamac@users.noreply.github.com>
2023-10-15 10:11:06 +0530

    Update Axios.js

--- src/utils/Axios.js
+++ src/utils/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://emart-server-rust.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
